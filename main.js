9d8ec7db9d7ac8e5a86a47ba1796a47044a0136b
DevWeb13 <rambo13010@hotmail.fr>
2021-07-27 15:43:48 +0200

    Sass + header

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
 const status = document.querySelector("#status");
+// const soundCoin = new Audio("./sound/Collect_Point_01.mp3");
+const soundCoin = () => {
+  const audio = new Audio();
+  audio.src = "./sound/Collect_Point_01.mp3";
+  audio.play();
+};
 
+// window.addEventListener("load", () => {
+//   soundCoin.play();
+// });
 document.addEventListener("DOMContentLoaded", () => {
   setTimeout(() => {
     status.style.opacity = 1;
+    soundCoin();
   }, 4500);
 });
